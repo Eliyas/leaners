cde1b9c64bcfc4d05a9deb747e3dd9ca5272556b
eliyas <saifeliyas@gmail.com>
2024-08-04 16:33:07 +0530

    list session

---
+++ sessions-notes/list/index-p2-04-08-2024.py
@@ -0,0 +1,64 @@
+
+
+myList = [2,4,5,6,7,8,9]
+print(2 in myList)
+
+
+# overried multiple items
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+
+#thislist[1:3] = ["blackcurrant", "watermelon"]
+
+thislist[3:5] = ["blackcurrant", "watermelon"]
+
+print(thislist)
+
+# without one value
+thislist2 = ["apple", "banana", "cherry", "orange"]
+
+thislist2[2:] = ["blackcurrant", "watermelon"]
+
+print(thislist2)
+
+# insert without replace
+
+""" class list:
+    def insert(startIndex, item):
+        return 0; """
+
+        
+thislist3 = ["apple", "banana", "cherry"]
+
+thislist3.insert(1, "watermelon")
+
+print(thislist3) 
+
+
+
+thislist4 = ["apple", "banana", "cherry"]
+thislist4.append("orange")
+print(thislist4)
+
+
+thislist5 = ["apple", "banana", "cherry"]
+
+thislist5.append("dragon")
+thislist5.append("berry")
+
+print(thislist5)
+
+#  extend
+
+myFruits = ["apple", "banana", "cherry"]
+myFruits2 = ["kiwi", "orange", "berry"]
+
+myFruits.extend(myFruits2)
+
+print(myFruits) 
+
+# removes first item that matches
+thislist = ["banana", "apple", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+
