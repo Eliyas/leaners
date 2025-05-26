9546b3ab89b9a7f41d283121ad3e2d53e6bf2684
Eliyas <saifeliyas@gmail.com>
2025-05-26 21:40:14 +0530

    reference

--- JS/Object/methods.js
+++ JS/Object/methods.js
@@ -15,8 +15,8 @@ const employee = {
     skills: ["JS", "Java", "HTML"]
 }
 
-console.log("Values 1 :", Object.values(employee))
-console.log("Keys 1 :", Object.keys(employee))
+// console.log("Values 1 :", Object.values(employee))
+// console.log("Keys 1 :", Object.keys(employee))
 
 
 // function declaration
@@ -39,5 +39,79 @@ function myKeys(employeeObj) {
 
 
 let result1 = myKeys(employee);
-console.log("Values 2 :", myValues(employee));
-console.log("Keys 2 :", result1);
+// console.log("Values 2 :", myValues(employee));
+// console.log("Keys 2 :", result1);
+
+
+// 1. Object reference
+
+// let name1 = "burhan";
+// let name2 = name1;
+// name2 = "basith";
+
+// console.log("name1 : ", name1);
+// console.log("name2 : ", name2);
+
+let employee1 = {
+    name: "burhan",
+    companyName: "Google",
+    companyPlace: "Chennai"
+}
+
+let employee2 = employee1;
+
+employee2.name = "basith";
+
+employee1.companyName = "TVS";
+
+console.log("employee1 : ", employee1.name);
+console.log("employee1 company : ", employee1.companyName);
+console.log("employee2 : ", employee2.name);
+console.log("employee2 company : ", employee2.companyName);
+
+
+// to cut the reference
+let employee3 = {
+    name: "burhan",
+    companyName: "Google",
+    companyPlace: "Chennai"
+}
+
+let employee4 = { ...employee3 };
+let employee5 = Object.create(employee3);
+
+employee4.name = "basith";
+
+employee3.companyName = "TVS";
+
+console.log("employee3 : ", employee3.name);
+console.log("employee3 company : ", employee3.companyName);
+console.log("employee4 : ", employee4.name);
+console.log("employee4 company : ", employee4.companyName);
+
+
+
+function myCreate1(object) {
+    let newObject = {};
+
+    // copy the properties from object to newObject
+    newObject = {...object};
+
+    return newObject;
+}
+
+
+function myCreate2(object) {
+    let newObject = {};
+
+    for(let key in object) {
+        console.log("Key : ", key);
+        console.log("value : ", object[key]);
+        newObject[key] = object[key];
+    }
+
+    return newObject;
+}
+
+console.log("copied Object : ", myCreate1(employee3));
+console.log("copied Object : ", myCreate2(employee3));
